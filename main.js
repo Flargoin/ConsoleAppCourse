0587542936b6a09f99717461d502be70cc14b3cb
Sergey <Flargoin@gmail.com>
2021-10-24 05:06:51 +0400

    Part 2 of practical work. Used loops, conditions, logical operators

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильм вы уже посмотрели','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели','');
+
+if (numberOfFilms < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms === 10 && numberOfFilms <= 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log ('Произошла ошибка');
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,14 +20,20 @@ let personalMovieDB = {
     privat: false
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
-const rating = prompt('На сколько оцените его?', '');
 
-const lastMovie1 = prompt('Один из последних просмотренных фильмов?', '');
-const rating1 = prompt('На сколько оцените его?', '');
+for( let i = 0; i < 2; i++) {
+    const lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
+          rating = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastMovie] = rating;
-personalMovieDB.movies[lastMovie1] = rating1;
+    if( lastMovie != null && rating != null && lastMovie != '' && rating != '' && lastMovie.length <= 50 ) {
+        personalMovieDB.movies[lastMovie] = rating;
+        console.log('Успешно');
+    } else {
+        console.log ('Произошла ошибка. Введите данные, название фильма не должно превышать 50 символов');
+    }
+}
+
+
 
 console.log(personalMovieDB);
 
